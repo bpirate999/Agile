cf60d44bc27d7e9471372a253847414a99fd2499
bpirate999 <borisov123@list.ru>
2013-11-22 18:14:04 +0400

    Changing marking of CSS

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,5 @@ validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
 validates :image_url, allow_blank: true, format: 
 {
-	with: %r{\Ahttps?:\/\/[^\n]\z}, 	message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'}
+	with: %r{(\W|^)[\w.+\-]{0,25}[\.](jp[e]{0,1}g|gif|png)(\W|$)}i, 	message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'}
 end
